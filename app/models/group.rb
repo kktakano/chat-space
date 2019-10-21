a4d5c02a837246825274736ecee554709e815395
kktakano <kktakano03@gmail.com>
2019-10-21 14:40:40 +0900

    マイグレーションのやり直し
    マイグレーションファイルに記述しなければいけないことをモデルファイルに記述してマイグレートしたので
    rollbackを実施して作成し直した

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,9 +1,3 @@
 class Group < ApplicationRecord
-  def change
-    create_table :groups do |t|
-      t.string :name, null: false
-      t.index :name, unique: true
-      t.timestamps
-    end
-  end
+  
 end
--- app/models/group_user.rb
+++ app/models/group_user.rb
@@ -1,8 +1,3 @@
 class GroupUser < ApplicationRecord
-  def change
-    create_table :group_users do |t|
-      t.references :group, foreign_key: true
-      t.references :user, foreign_key: true
-      t.timestamps
-  end
+  
 end
