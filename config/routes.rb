29c91fdd6f2e95227b406b7f14bfeeea9b09e610
kktakano <kktakano03@gmail.com>
2019-10-21 19:33:26 +0900

    ルートパスの変更、ビューファイルの移動

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  root 'messages#index'
+  root 'groups#index'
   devise_for :users
   resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
