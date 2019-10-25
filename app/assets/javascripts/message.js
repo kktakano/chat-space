076a17905b8167bf0e8dad159bfc0b8b6729ec7f
kktakano <kktakano03@gmail.com>
2019-10-25 21:42:47 +0900

    jbuilderの変更とdoneメソッドの作成

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,27 @@
 $(function(){
+  function buildHTML(message){
+    var message_content = message.content?
+                              `${message.content}`:"";
+    var message_image = message.image?
+                              `<img src="${message.image}">`:"";
+
+    var html = `<div class="message">
+                  <div class="message__upper-info">
+                    <div class="message__upper-info__tolker">
+                      ${message.user_name} 
+                    </div>
+                    <div class="message__upper-info__date">
+                      ${message.created_at}
+                    </div>
+                  </div>
+                  <div class="message__text">
+                    ${message_content}
+                    ${message_image}
+                  </div>
+                </div>`
+    return html;
+  }
+
   $('.new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -11,5 +34,10 @@ $(function(){
       processData: false,
       contentType: false
     })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.form__message').val('');
+    })
   })
-})
+});
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,4 +1,4 @@
-json.user.name @message.user.name
-json.created_at @message.created_at
+json.user_name @message.user.name
+json.created_at @message.created_at.strftime("%Y/%m/%d %H:%M")
 json.content @message.content
-json.image @message.image
+json.image @message.image.url
