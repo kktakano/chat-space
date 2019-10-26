dccaf57d9eb911e5106a57a8626b97487bbc8c08
kktakano <kktakano03@gmail.com>
2019-10-26 17:45:06 +0900

    作成したhtmlをビューに描写

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,5 +1,22 @@
 $(function(){
 
+  var search_list = $("#user-search-result")
+
+  function appendUser(user){
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+                </div>`
+
+    search_list.append(html);
+  }
+  function appendErrMsgToHTML(msg) {
+    var html = `<div class="chat-group-user clearfix">
+                 <p class="chat-group-user__name">${msg}</p>
+                </div>`
+    search_list.append(html);
+  }
+
   $("#user-search-field").on("keyup", function(){
     var input = $("#user-search-field").val();
     $.ajax({
@@ -9,7 +26,14 @@ $(function(){
       dataType: 'json',
     })
     .done(function(users){
-      console.log("成功です");
+      search_list.empty();
+      if (users.length !== 0) {
+        users.forEach(function(user){
+          appendUser(user);
+        });
+      } else {
+        appendErrMsgToHTML("ユーザーが見つかりません");
+      }
     })
     .fail(function(){
       console.log("失敗です");
