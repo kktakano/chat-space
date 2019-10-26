88e90846c47a05a15ff828f3305fd709ff6ff6ed
kktakano <kktakano03@gmail.com>
2019-10-26 19:12:31 +0900

    jsonデータを使用してhamlが描写されるよう実装
    入力がない時の条件分岐追加できていなかったのであとで追加
    エラー時の処理まで実装

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,42 +1,48 @@
-$(function(){
+document.addEventListener('turbolinks:load', function(){
+  $(function(){
 
-  var search_list = $("#user-search-result")
-
-  function appendUser(user){
-    var html = `<div class="chat-group-user clearfix">
-                  <p class="chat-group-user__name">${user.name}</p>
-                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
-                </div>`
-
-    search_list.append(html);
-  }
-  function appendErrMsgToHTML(msg) {
-    var html = `<div class="chat-group-user clearfix">
-                 <p class="chat-group-user__name">${msg}</p>
-                </div>`
-    search_list.append(html);
-  }
-
-  $("#user-search-field").on("keyup", function(){
-    var input = $("#user-search-field").val();
-    $.ajax({
-      type: 'GET',
-      url: '/users',
-      data: {keyword: input},
-      dataType: 'json',
-    })
-    .done(function(users){
-      search_list.empty();
-      if (users.length !== 0) {
-        users.forEach(function(user){
-          appendUser(user);
-        });
-      } else {
-        appendErrMsgToHTML("ユーザーが見つかりません");
-      }
-    })
-    .fail(function(){
-      console.log("失敗です");
+    var search_list = $("#user-search-result")
+  
+    function appendUser(user){
+      var html = `<div class="chat-group-user clearfix">
+                    <p class="chat-group-user__name">${user.name}</p>
+                    <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+                  </div>`;
+  
+      search_list.append(html);
+    }
+    function appendErrMsgToHTML(msg) {
+      var html = `<div class="chat-group-user clearfix">
+                   <p class="chat-group-user__name">${msg}</p>
+                  </div>`;
+      search_list.append(html);
+    }
+  
+    $("#user-search-field").on("keyup", function(){
+      var input = $("#user-search-field").val();
+      $.ajax({
+        type: 'GET',
+        url: '/users',
+        data: {keyword: input},
+        dataType: 'json',
+      })
+      .done(function(users){
+        search_list.empty();
+        if (users.length !== 0) {
+          users.forEach(function(user){
+            appendUser(user);
+          });
+        }else if (input.length == 0){
+          return false;
+        } else {
+          appendErrMsgToHTML("ユーザーが見つかりません");
+        }
+      })
+      .fail(function(){
+        alert("ユーザー検索に失敗しました");
+      });
     });
   });
+
+  
 });
