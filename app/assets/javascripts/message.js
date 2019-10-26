d0d4eb90d8c4679738ddc85245fe48f8a63e88ec
kktakano <kktakano03@gmail.com>
2019-10-26 12:12:39 +0900

    グループ切り替え後送信時のリロード無効化
    コントローラの修正も

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,50 +1,53 @@
-$(function(){
-  function buildHTML(message){
-    var message_content = message.content?
-                              `${message.content}`:"";
-    var message_image = message.image?
-                              `<img src="${message.image}">`:"";
 
-    var html = `<div class="message">
-                  <div class="message__upper-info">
-                    <div class="message__upper-info__tolker">
-                      ${message.user_name} 
+document.addEventListener('turbolinks:load', function(){
+  $(function(){
+    function buildHTML(message){
+      var message_content = message.content?
+                                `${message.content}`:"";
+      var message_image = message.image?
+                                `<img src="${message.image}">`:"";
+  
+      var html = `<div class="message">
+                    <div class="message__upper-info">
+                      <div class="message__upper-info__tolker">
+                        ${message.user_name} 
+                      </div>
+                      <div class="message__upper-info__date">
+                        ${message.created_at}
+                      </div>
                     </div>
-                    <div class="message__upper-info__date">
-                      ${message.created_at}
+                    <div class="message__text">
+                      ${message_content}
+                      ${message_image}
                     </div>
-                  </div>
-                  <div class="message__text">
-                    ${message_content}
-                    ${message_image}
-                  </div>
-                </div>`
-    return html;
-  }
-
-  $('.new_message').on('submit', function(e){
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr('action')
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
+                  </div>`
+      return html;
+    }
+  
+    $('.new_message').on('submit', function(e){
+      e.preventDefault();
+      var formData = new FormData(this);
+      var url = $(this).attr('action')
+      $.ajax({
+        url: url,
+        type: "POST",
+        data: formData,
+        dataType: 'json',
+        processData: false,
+        contentType: false
+      })
+      .done(function(data){
+        var html = buildHTML(data);
+        $('.messages').append(html);
+        $('.form__message').val('');
+        $('.hidden').val('');
+        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+        $('.form__submit').prop('disabled', false);
+      })
+      .fail(function() {
+        alert('error');
+        $('.form__submit').prop('disabled', false);
+      });
     })
-    .done(function(data){
-      var html = buildHTML(data);
-      $('.messages').append(html);
-      $('.form__message').val('');
-      $('.hidden').val('');
-      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
-      $('.form__submit').prop('disabled', false);
-    })
-    .fail(function() {
-      alert('error');
-      $('.form__submit').prop('disabled', false);
-    });
-  })
+  });
 });
