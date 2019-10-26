233f5a6870aa520afe6e6fab401313878760342a
kktakano <kktakano03@gmail.com>
2019-10-27 02:13:24 +0900

    新規チャットグループ画面の実装
    groupのnameに対するバリデーションで一位制約が不足していた

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -8,7 +8,6 @@ document.addEventListener('turbolinks:load', function(){
                     <p class="chat-group-user__name">${user.name}</p>
                     <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
                   </div>`;
-  
       search_list.append(html);
     }
     function appendErrMsgToHTML(msg) {
@@ -16,6 +15,17 @@ document.addEventListener('turbolinks:load', function(){
                    <p class="chat-group-user__name">${msg}</p>
                   </div>`;
       search_list.append(html);
+    }
+    function addDeleteUser(name, id){
+      var html = `<div class="chat-group-user clearfix" id="${id}">
+                    <p class="chat-group-user__name">${name}</p>
+                    <div class="user-search-remove chat-group-user__btn chat-group-user__btn--add" data-user-id="${id}" data-user-name="${name}">削除</div>
+                  </div>`;
+      $(".js-add-user").append(html);
+    }
+    function addMember(userId){
+      var html = `<input value="${userId}" name="group[user_ids][]" type="hidden" id="group_user_ids_${userId}" />`;
+      $(`#${userId}`).append(html);
     }
   
     $("#user-search-field").on("keyup", function(){
@@ -43,9 +53,19 @@ document.addEventListener('turbolinks:load', function(){
       });
     });
     $(document).on('click', '.user-search-add', function(){
-      var result = $('.user-search-add').data();
-      
-    })
+      const userName = $(this).attr("data-user-name");
+      const userId = $(this).attr("data-user-id");
+      $(this)
+        .parent()
+        .remove();
+      addDeleteUser(userName, userId);
+      addMember(userId);
+    });
+    $(document).on("click", ".user-search-remove", function(){
+      $(this)
+        .parent()
+        .remove();
+    });
   });
 
   
